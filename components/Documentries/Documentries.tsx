de68fe948be49de06319c68c4a6903e39e30a2d9
ayazdattuu101 <ayazdattuu101@gmail.com>
2022-11-22 17:03:24 +0500

    changes

--- components/Documentries/Documentries.tsx
+++ components/Documentries/Documentries.tsx
@@ -11,19 +11,18 @@ const Documentries = () => {
       <div className="px-4">
         <h2 className="text-3xl font-bold mb-6">MEDIA / DOCUMENTARIES</h2>
       </div>
-      <div className="lg:flex items-center md:justify-center justify-center mx-auto flex-rows grid md:grid-cols-2 justify-evenly">
+      <div className="lg:flex items-center md:justify-center justify-center mx-auto flex-rows grid md:grid-cols-2 lg:justify-evenly">
       <div className="bg-playerbg px-3 py-10 ml-2 mr-2 mt-10 mb-10">
           <Image src="/../public/imam.png" width={450} height={100} alt="alt" className="mx-auto"/>
-          <p className="mt-2 text-xl font-medium">Changing Valleys</p>
+          <p className="mt-2 text-xl font-bold">Changing Valleys</p>
         </div>
         <div className="bg-playerbg px-3 py-10 ml-2 mr-2 mt-10 mb-10">
-          <Image src="/../public/imam.png" width={450} height={100} alt="alt" className="mx-auto"/>
-          <p className="mt-2 text-xl font-medium">Changing Valleys</p>
+          <Image src="/../public/image14.png" width={450} height={100} alt="alt" className="mx-auto"/>
+          <p className="mt-2 text-xl font-bold">Harnesing The Elements</p>
         </div>
         <div className="bg-playerbg px-3 py-10 ml-2 mr-2 mt-10 mb-10">
-          <Image src="/../public/imam.png" width={450} height={100} alt="alt"className="mx-auto" />
-
-          <p className="mt-2 text-xl font-medium ">Changing Valleys</p>
+          <Image src="/../public/image15.png" width={450} height={100} alt="alt"className="mx-auto" />
+          <p className="mt-2 text-xl font-bold">My Dream</p>
         </div>
       </div>
     </div>
