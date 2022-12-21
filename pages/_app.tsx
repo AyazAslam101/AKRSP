99d6fdd4574aad0ad2a8f8184b5f6fe24dd51529
AyazAslam101 <ayazdattuu101@gmail.com>
2022-12-21 18:52:43 +0500

    fix hyrdration ui error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
+import { useEffect, useState } from 'react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [showChild, setShowChild] = useState(false)
+
+  useEffect(() => {
+    setShowChild(true)
+  }, [])
+
+  if (!showChild) {
+    return null
+  }
   return <Component {...pageProps} />
 }
