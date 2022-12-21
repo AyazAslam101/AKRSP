cf378327c9aacd23dafe1bbbc7fbe7f068c6dfe8
AyazAslam101 <ayazdattuu101@gmail.com>
2022-12-22 00:05:30 +0500

    changes in sidebar

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -10,48 +10,48 @@ function SideBar() {
 
   return (
     <div className="fixed right-0 top-[28rem] bottom-0 xl:block hidden z-10">
-    <div
-      className="bg-primary py-3 w-12 grid grid-rows-3 justify-center gap-6 rounded"
-      data-aos-duration="800"
-      data-aos="flip-up"
-    >
-      <div>
-        <button className="cursor-pointer w-10  flex justify-center">
-          <a href="#">
-            <User />
-          </a>
-        </button>
-      </div>
-      <div>
-        <button className="cursor-pointer w-10 flex justify-center">
-          <a href="#">
-            <Vector />
-          </a>
-        </button>
-      </div>
-      <div>
-        <button className="cursor-pointer w-10 flex justify-center">
-          <a href="#">
-            <MailSvg />
-          </a>
-        </button>
-      </div>
-      <div>
-        <button className="cursor-pointer w-10 flex justify-center">
-          <a href="#">
-            <Display />
-          </a>
-        </button>
-      </div>
-      <div>
-        <button className="cursor-pointer w-10 flex justify-center">
-          <a href="#">
-            <PhoneSvg />
-          </a>
-        </button>
+      <div
+        className="bg-primary py-3 w-12 grid grid-rows-3 justify-center gap-6 rounded"
+        data-aos-duration="800"
+        data-aos="flip-up"
+      >
+        <div>
+          <button className="cursor-pointer w-10  flex justify-center">
+            <a href="#">
+              <User />
+            </a>
+          </button>
+        </div>
+        <div>
+          <button className="cursor-pointer w-10 flex justify-center">
+            <a href="#">
+              <Vector />
+            </a>
+          </button>
+        </div>
+        <div>
+          <button className="cursor-pointer w-10 flex justify-center">
+            <a href="#">
+              <MailSvg />
+            </a>
+          </button>
+        </div>
+        <div>
+          <button className="cursor-pointer w-10 flex justify-center">
+            <a href="#">
+              <Display />
+            </a>
+          </button>
+        </div>
+        <div>
+          <button className="cursor-pointer w-10 flex justify-center">
+            <a href="#">
+              <PhoneSvg />
+            </a>
+          </button>
+        </div>
       </div>
     </div>
-</div>
   );
 }
 export default SideBar;
